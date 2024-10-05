572cdd4868dfea5bd78fa95e234a18aea863068c
Hector-Lozada <eeaae7570@gmail.com>
2024-10-05 13:52:40 -0500

    update

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export const ProductShowcase = () => {
   return (
-    <section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF]'>
+    <section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'>
       <div className="container">
         <div className='max-w-[540px] mx-auto'>
           <div className='flex justify-center'>
@@ -19,7 +19,7 @@ export const ProductShowcase = () => {
           <Image src={WorkSpace} alt='workspace image' className='mt-10'
           priority={true}
           quality={100}/>
-          <Image src={gr} alt='gr photo' height={148} className='absolute -right-36 -top-32'/>
+          <Image src={gr} alt='gr photo' height={148} className=' hidden md:block  absolute -right-36 -top-32'/>
         </div>
       </div>
     </section>
