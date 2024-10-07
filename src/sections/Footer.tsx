2ad3f1ac25e36f01ea8a5660135a291238da8472
Hector-Lozada <eeaae7570@gmail.com>
2024-10-06 20:11:12 -0500

    hh

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,29 @@
+import logo from '@/assets/logo.png';
+import Image from 'next/image';
+import SocialInsta from '@/assets/social-insta.svg';
+import SocialWha from '@/assets/social-whatsapp.svg';
+import SocialTele from '@/assets/social-telegram.svg';
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer className='bg-white border text-black text-sm py-10 text-center mt-5'>
+      <div className='container'>
+        <div className='inline-flex relative'>
+          <Image src={logo} height={160} width={160} alt='Logo ETG'/>
+        </div>
+        <nav>
+          <a href="#">Sobre Nosotros</a>
+          <a href="#">Características</a>
+          <a href="#">Clientes</a>
+          <a href="#">Precios</a>
+          <a href="#">FAQ</a>
+          <a href="#">Carrera</a>
+        </nav>
+        <SocialInsta/>
+        <SocialWha/>
+        <SocialTele/>
+      </div>
+      <p>&copy; 2024 Elegance Trading Group. Todos los derechos reservados.</p>
+    </footer>
+  );
 };
