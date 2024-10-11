ba05a0e27260e422c5996901024ae1cf8634dba8
Hector-Lozada <eeaae7570@gmail.com>
2024-10-11 14:36:07 -0500

    layout add adsence

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import Head from "next/head"; // Importa Head para manejar el head del documento
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
@@ -17,6 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="relative">
+      <Head>
+        <title>Elegance Trading Group</title>
+        <meta name="description" content="Mejora tu estado financiero" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4480402395128186"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body className={clsx(dmSans.className, "")}>
         {children}
       </body>
